35cc60d82b9f7826b22a551950eb22e7ee1e5888
Gulden Maratovna <55193105+zhgulden@users.noreply.github.com>
2020-05-15 03:17:40 +0600

    Add gauss.py
    The program compares the runtime of my implementation of the algorithm and the function from scipy.linalg, and also displays a graph

---
+++ Linear system of equations/gauss.py
@@ -0,0 +1,75 @@
+import numpy as np
+import time
+import scipy.linalg as sl
+import matplotlib.pyplot as plt
+
+def forward(A, f, matrixSize):
+    for k in range(matrixSize):
+        A[k] = A[k] / A[k][k]
+        f[k] = f[k] / A[k][k] 
+        
+        for i in range(k + 1, matrixSize):
+            A[i] = A[i] - A[k] * A[i][k]
+            f[i] = f[i] - f[k] * A[i][k]
+            A[i][k] = 0
+    return A, f
+
+def backward(A, f, matrixSize):
+    myAnswer = [0] * matrixSize
+    for i in range(matrixSize - 1, -1, -1):
+        myAnswer[i] = f[i]
+        for j in range(i + 1, matrixSize):
+            myAnswer[i] = myAnswer[i] - A[i][j] * myAnswer[j]
+    return np.array(myAnswer)
+
+def gauss(A, f, matrixSize):
+    A, f = forward(A, f, matrixSize)
+    myAnswer = backward(A, f, matrixSize)
+    return myAnswer
+
+def generate_random_data(matrixSize):
+    A = np.random.rand(matrixSize, matrixSize)
+    f = np.random.rand(matrixSize)
+    return A, f
+
+def get_time_data(matrixSize):
+    myTime = 0.0
+    linalgTime = 0.0
+    iterator = 0
+    while matrixSize <= 1000:
+        A, f = generate_random_data(matrixSize)
+        startTime = time.time()
+        myAnswer = gauss(A, f, matrixSize)
+        myTime = time.time() - startTime
+        startTime = time.time()
+        linalgAnswer = np.linalg.solve(A, f)
+        linalgTime = time.time() - startTime
+        myTimeData[iterator] = round(myTime, 10)
+        linalgTimeData[iterator] = round(linalgTime, 10)
+        matrixSize += 10
+        iterator += 1
+    return myTimeData, linalgTimeData
+
+def show_result(myTimeData, linalgTimeData):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set(xlim = [0, 90],
+        ylim = [0.0, 1.0],
+        title = 'Red - scipy.linalg.solve_banded(), blue - gauss()',
+        xlabel = 'matrix size (thousand)',
+        ylabel = 'time (seconds)')
+    ax.plot(myTimeData, color = 'b')
+    ax.plot(linalgTimeData, color = 'r')
+    plt.savefig('gauss.png', bbox_inches='tight')
+    plt.show()
+
+matrixSize = 100
+
+numberOfData = 100
+myAnswer = np.zeros(matrixSize)
+linalgAnswer = np.zeros(matrixSize)
+myTimeData = np.zeros(numberOfData)
+linalgTimeData = np.zeros(numberOfData)
+myTimeData, linalgTimeData = get_time_data(matrixSize)
+show_result(myTimeData, linalgTimeData)
+
