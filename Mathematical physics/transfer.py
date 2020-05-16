e4a35cbaef35105ecebc4bb8e88ffddc9691fe1b
Gulden Maratovna <55193105+zhgulden@users.noreply.github.com>
2020-05-16 15:42:19 +0600

    Update transfer.py

--- Mathematical physics/transfer.py
+++ Mathematical physics/transfer.py
@@ -17,29 +17,29 @@ m = 100
 h = (R - L) / n
 tau = T / m
 
-x = np.linspace (L , R , n + 1)
-t = np.linspace (0.0 , T , m + 1)
-y = np.zeros (( m + 1 , n + 1))
+x = np.linspace(L, R, n + 1)
+t = np.linspace (0.0, T, m + 1)
+y = np.zeros((m + 1, n + 1))
 
 d = C * tau / h
 y[0] = np.vectorize(u0)(x)
-for k in range (m):
+for k in range(m):
 	for i in range(1, n + 1):
-		y [ k +1][ i ] = y [ k ][ i ] - d * ( y [ k ][ i ] - y [ k ][ i -1])
+		y[k + 1][i] = y[k][i] - d * (y[k][i] - y [k][i - 1])
 
-vsolution = np.vectorize(solution, excluded = [ 't ' ])
-u = np . zeros (( m + 1 , n + 1))
-for k in range ( m ):
-	u [ k ] = vsolution (x , tau * k )
+vsolution = np.vectorize(solution, excluded = ['t'])
+u = np.zeros((m + 1, n + 1))
+for k in range(m):
+	u[k] = vsolution(x, tau * k)
 
-def animate ( k ):
-	plt.clf ()
-	plt.ylim (0 , 1.25)
-	plt.title ( "time = " + str ( tau * k ) + " seconds")
-	plt.plot (x , y [ k ], label = 'Numerical')
-	plt.plot (x , u [ k ], label = 'Analitical')
-	plt.legend ()
+def animate (k):
+	plt.clf()
+	plt.ylim(0, 1.25)
+	plt.title ("time = " + str(tau * k) + " seconds")
+	plt.plot(x, y[k], label = 'Numerical')
+	plt.plot(x, u[k], label = 'Analitical')
+	plt.legend()
 	
-ani = animation.FuncAnimation ( plt.figure (0) , animate , frames = y.shape [0] , interval =100)
-ani.save ('heat.gif', writer='imagemagick', fps = 15)
-plt.show ()
+ani = animation.FuncAnimation(plt.figure(0), animate, frames = y.shape[0], interval = 100)
+ani.save('transfer.gif', writer='imagemagick', fps = 15)
+plt.show()
